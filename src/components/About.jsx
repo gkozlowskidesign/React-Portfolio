25d890f5451f67e2ef37ce50f81695e4a65312b5
DefiLord <gkozlowskibusiness@outlook.com>
2022-10-21 00:09:33 -0400

    project done

---
+++ src/components/About.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+            <div className="pb-8">
+                <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
+            </div>
+            <p className="text-xl mt-20">
+                My background experience has a focus on managing, designing or 
+                developing SaaS and PaaS products or project features 
+                with freelance design experience creating modern UI expereinces. 
+                I've worked with startups, major corporate 
+                enterprises and small local businesses satisfying stakeholders 
+                and reaching roadmap goals with proven results.
+
+            </p>
+            <br/>
+
+            <p className="text-xl">
+            I am exceptionally good at software project management from being a team 
+            player and supporting members during tight bottleneck sprints helping 
+            consistently optimize and adapt to the work-process, managing workflow 
+            collaboratively.  By takeing pride in being a team player and focusing 
+            on communication, organization, planning, following through with strategy, 
+            holding people accountable, I have delivered results with accuracy and 
+            reliability over and over again always looking for feedback and ways I can grow. 
+            I work best with High-Level Managers and Developers by briding the gap between 
+            technicality developers communicate with and breaking it down in generality. 
+            Products or projects I've work on pertain to software-engineering, 
+            cyber-security, cloud-computing, business-operations and marketing. 
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
