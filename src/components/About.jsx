5172d8531ac5fb67a4d5b9417b70ca0fcfef5924
DefiLord <gkozlowskibusiness@outlook.com>
2022-10-21 12:52:17 -0400

    project done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,25 +11,13 @@ const About = () => {
                 My background experience has a focus on managing, designing or 
                 developing SaaS and PaaS products or project features 
                 with freelance design experience creating modern UI expereinces. 
-                I've worked with startups, major corporate 
-                enterprises and small local businesses satisfying stakeholders 
-                and reaching roadmap goals with proven results.
-
             </p>
             <br/>
 
             <p className="text-xl">
-            I am exceptionally good at software project management from being a team 
-            player and supporting members during tight bottleneck sprints helping 
-            consistently optimize and adapt to the work-process, managing workflow 
-            collaboratively.  By takeing pride in being a team player and focusing 
-            on communication, organization, planning, following through with strategy, 
-            holding people accountable, I have delivered results with accuracy and 
-            reliability over and over again always looking for feedback and ways I can grow. 
-            I work best with High-Level Managers and Developers by briding the gap between 
-            technicality developers communicate with and breaking it down in generality. 
-            Products or projects I've work on pertain to software-engineering, 
-            cyber-security, cloud-computing, business-operations and marketing. 
+                I've worked with startups, major corporate 
+                enterprises and small local businesses satisfying stakeholders 
+                and reaching roadmap goals with proven results.
             </p>
         </div>
     </div>
