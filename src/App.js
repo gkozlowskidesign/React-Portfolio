de2fd4e6f5504587facb540f98acf755f345e092
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-11-28 15:25:55 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import NavBar from "./components/NavBar";
-import Home from "./components/Home";
-import SocialLinks from "./components/SocialLinks";
-import About from "./components/About";
-import Portfolio from "./components/Portfolio";
-import Experience from "./components/Experience";
-import Contact from "./components/Contact";
+import NavBar from "./layouts/Navigation";
+import Home from "./pages/Overview/Home";
+import Experience from "./pages/Overview/Experience";
+import Social from "./pages/Connect/SocialLinks";
+import Portfolio from "./pages/Overview/Portfolio";
+import Contact from "./pages/Overview/Contact";
+import Projects from "./pages/Publications/Publications-Medium";
+import Wine from "./pages/Portfolio/Project-LiquorStore";
+import Restaurant from "./pages/Portfolio/Project-Restaurant";
+import Metaverse from "./pages/Portfolio/Project-Metaverse";
+import Dashboard from "./pages/Portfolio/Project-Dashboard";
+import Iteration from "./pages/Portfolio/Project-Portfolio";
+import Health from "./pages/Portfolio/Project-Health";
+import Publications from "./pages/Overview/Publications";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
-      <Portfolio />
-      <Experience />
-      <Contact />
-
-      <SocialLinks />
+    <Router>
+    <div className="App">
+      <div className="content">
+        <Switch>
+          <Route exact path="/overview">
+          <NavBar/>
+          <Home/>
+          <Publications/>
+          <Experience/>
+          <Portfolio/>
+          <Social/>
+          <Contact/>
+          </Route>
+          <Route  path="/background/education">
+          <NavBar/>
+          <Experience/>
+          <Social/> 
+          </Route>
+          <Route  path="/background/experience">
+          <NavBar/>
+          <Experience/> 
+          <Social/>
+          </Route>
+          <Route  path="/background/freelance">
+          <NavBar/>
+          <Experience/> 
+          <Social/>
+          </Route>
+          <Route  path="/project/documentation/liquorstore/website">
+          <NavBar/>
+          <Wine/> 
+          </Route>
+          <Route  path="/project/documentation/dashboard/user-interface">
+          <NavBar/>
+          <Dashboard/> 
+          </Route>
+          <Route  path="/project/documentation/portfolio/website">
+          <NavBar/>
+          <Iteration/> 
+          </Route>
+          <Route  path="/project/documentation/restaurant/website">
+          <NavBar/>
+          <Restaurant/> 
+          </Route>
+          <Route  path="/project/documentation/healthcare/web-application">
+          <NavBar/>
+          <Health/> 
+          </Route>
+          <Route  path="/project/documentation/software/user-interface">
+          <NavBar/>
+          <Metaverse/> 
+          </Route>
+          <Route  path="/project/documentation/software/website">
+          <NavBar/>
+          <Metaverse/> 
+          </Route>
+          <Route  path="/publications">
+          <NavBar/>
+          <Projects/> 
+          <Social/>
+          </Route>
+      </Switch>
+      </div>
     </div>
+    </Router>
   );
 }
+
 export default App;
