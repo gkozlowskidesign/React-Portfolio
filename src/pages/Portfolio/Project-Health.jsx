db96fa353c4357474116090a7f66e9e05a52d289
Gary Kozlowski <82541715+gkozlowskidesign@users.noreply.github.com>
2022-11-29 13:07:45 -0500

    Update Project-Health.jsx

--- src/pages/Portfolio/Project-Health.jsx
+++ src/pages/Portfolio/Project-Health.jsx
@@ -7,7 +7,7 @@ const products = [
     {
       id: 1,
       name: 'Requirements',
-      description: "Motivated, passionate, and highly driven Project Manager and Developer with in depth experience working in a wide variety of technical environments. Comprehensive knowledge of configuring, managing, optimizing, and  troubleshooting software, marketing, and entire IT infrastructures at small to medium scale. Comprehensive knowledge of configuring, managing, optimizing, and  troubleshooting software, marketing, and entire IT infrastructures at small to medium scale.",
+      description: "Coming Soon....",
       href: 'https://mirror.xyz/0x38034A81D48cBf8D4c4C757856d14492C848015b/9StXr96qlubzk57TmUcAxv6L9E7PHj_T3vlGytGs_4k',
       imageSrc: 'https://mirror-media.imgix.net/publication-images/w8ZcP23A9IAKFGrT-YN3s.jpeg?height=1400&width=2800&h=1400&w=2800&auto=compress',
       imageAlt: 'web3 concepts',
@@ -15,7 +15,7 @@ const products = [
     {
         id: 9,
         name: 'Development',
-        description: "Motivated, passionate, and highly driven Project Manager and Developer with in depth experience working in a wide variety of technical environments. Comprehensive knowledge of configuring, managing, optimizing, and  troubleshooting software, marketing, and entire IT infrastructures at small to medium scale. Comprehensive knowledge of configuring, managing, optimizing, and  troubleshooting software, marketing, and entire IT infrastructures at small to medium scale.",
+        description: "Coming Soon....",
         href: 'https://mirror.xyz/0x38034A81D48cBf8D4c4C757856d14492C848015b/e3ekBjU5NnKr087koz4oaFMnKi3JQO0MTrsNw9c1tRc',
         imageSrc: 'https://mirror-media.imgix.net/publication-images/jhEW9kY3ukKX4zR-fXQg1.jpeg?height=1400&width=2800&h=1400&w=2800&auto=compress',
         imageAlt: 'web3 projects',
@@ -23,7 +23,7 @@ const products = [
     {
       id: 3,
       name: 'Management',
-      description: "Motivated, passionate, and highly driven Project Manager and Developer with in depth experience working in a wide variety of technical environments. Comprehensive knowledge of configuring, managing, optimizing, and  troubleshooting software, marketing, and entire IT infrastructures at small to medium scale. Comprehensive knowledge of configuring, managing, optimizing, and  troubleshooting software, marketing, and entire IT infrastructures at small to medium scale.",
+      description: "Coming Soon....",
       href: 'https://mirror.xyz/0x38034A81D48cBf8D4c4C757856d14492C848015b/89q_BcgSPYyYStmMj-q_f8m1yT_wj1Grpd27KeUVs6c',
       imageSrc: 'https://mirror-media.imgix.net/publication-images/5oRxqPQ-3f__rdIEkK7up.jpeg?height=1400&width=2800&h=1400&w=2800&auto=compress',
       imageAlt: 'web3 concepts',
@@ -104,33 +104,12 @@ const products = [
     </div>
 
     
-<hr></hr>
 
-    <div className='p-14 mt-16 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 gap-10 mb-10 rounded-3xl'>
-
-{products.map((product) => (
-<a key={product.id} href={product.href} className="group rounded-3xl bg-gray-900">
-<div className="aspect-w-1 aspect-h-1 w-full mx-auto overflow-hidden rounded-3xl xl:aspect-w-7 xl:aspect-h-8">
-<h3 className="mt-4 p-4 text-2xl text-center font-bold text-white">{product.name}</h3>
-</div>
-
-
-
-<h3 className="mt-4 p-8  bg-gray-800 text-gray-400 ring-4 ring-gray-900 rounded-3xl"><span className='text-white text-xl font-extrabold'>Description </span> {product.description}</h3>
-
-</a>
-))}
-
-</div>
     </section>
 </div>
 </div>
 )
 }
-
-
-     
-        
 
 
      
